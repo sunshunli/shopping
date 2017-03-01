bb698ab46c6af454eefb89adfc4c524f0a306f08
sunshunli <sunshunli@hotmail.com>
2017-03-01 14:25:18 +0800

    添加商品

--- src/admin/js/goods.js
+++ src/admin/js/goods.js
@@ -2,14 +2,57 @@
 
     var $dlg = $('#goodsDlg');
 
+    //程序唯一入口
     var init = function(argument) {
         initEvent();
+        getTableData();
     };
 
     var initEvent = function() {
         $('#newBtn').on('click', onNewBtnClick);
         $('#saveBtn').on('click', onSaveBtnClick);
     };
+
+    //渲染表格
+    var getTableData = function(argument) {
+        var url = '../../../api/shopping_goods_list.php';
+
+        //弹出遮罩层
+        layer.load(0, {shade: [0.3,'#000']}); //0代表加载的风格，支持0-2
+
+        $.get(url, function(response) {
+            if (response.success) {
+                renderTable(response);
+                //关闭遮罩层
+                layer.closeAll('loading');
+            }
+        }, 'json');
+    }
+
+    var renderTable = function(response) {
+        //获得数据数组
+        var data = response.data,
+            trArr = [];
+        //遍历数组
+        $.each(data, function(index, obj) {
+            trArr.push(
+                '<tr>',
+                    '<td><input type="checkbox"></td>',
+                    '<td>', index + 1, '</td>',
+                    '<td>', obj.title,'</td>',
+                    '<td>￥', obj.price,'</td>',
+                    '<td>', obj.details,'</td>',
+                    '<td>', obj.amount,'</td>',
+                    '<td>', obj.classify,'</td>',
+                '</tr>'
+            );
+        });
+
+        $('#goodsTable tbody').html(trArr.join(''));
+
+    }
+
+
 
     var onNewBtnClick = function() {
         $dlg.modal({
@@ -50,16 +93,17 @@
             return;
         }
         //弹出遮罩层
-        var index = layer.load(0, {shade: [0.3,'#fff']}); //0代表加载的风格，支持0-2
+        var index = layer.load(0, {shade: [0.3,'#000']}); //0代表加载的风格，支持0-2
 
         //Ajax请求
         $.get(url, data, function(response) {
             if (response.success) {
                 //关闭遮罩层
-                layer.closeAll('loading');
+                layer.closeAll();
                 $dlg.modal('hide');
                 $('#gForm').trigger('reset');
                 layer.msg('商品添加成功！', {offset: 't', anim: 0});
+                getTableData();
             } else {
                 alert('商品添加失败！');
             }
